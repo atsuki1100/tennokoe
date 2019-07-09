13f20f8662c721687e45cd7a9032c438fba14423
atsuki1100 <atuki0303@outlook.jp>
2019-07-09 14:38:03 +0900

    Answer Update

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,21 @@
 class AnswersController < ApplicationController
+  before_action :set_question
 
   def create
-    @answer = Answer.new(comment_params)
+    @answer = @question.answers.new(answer_params)
+    @answer.user_id = current_user.id
+    @answer.save
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:body).merge(question_id: @question)
+  end
+
+  def set_question
+    @question = Question.find(params[:question_id])
+  end
 
 end

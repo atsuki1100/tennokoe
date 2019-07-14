a09e1a04a389d687d45dd439e0f8ffab6e57423f
atsuki1100 <atuki0303@outlook.jp>
2019-07-14 21:41:41 +0900

    kaiten Update

---
+++ app/assets/javascripts/question.js
@@ -0,0 +1,9 @@
+// $(function() {
+//   $(document).on('mouseover', '.nikoniko', function() {
+//     $('.nikoniko').animate({
+//       'left': '500px'
+//     },{
+//       'duration': 1000,
+//      });
+//   });
+// });
